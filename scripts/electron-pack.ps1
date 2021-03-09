cc216e6c3fb108a6d085dc85c5c4acc644b14c9b
Hyun Seungmin <boscohyun@gmail.com>
2021-03-09 15:15:36 +0900

    Copy ./logo-launcher.ico to ./pack/icon.ico

--- scripts/electron-pack.ps1
+++ scripts/electron-pack.ps1
@@ -1,3 +1,8 @@
 #!/usr/bin/env pwsh
 $hash = git rev-parse HEAD
-npx electron-packager ./dist --out ./pack --overwrite --icon=./logo-launcher.ico --app-version=$env:npm_package_version-$($hash.Substring(0,8))
+npx electron-packager ./dist `
+  --out ./pack `
+  --overwrite `
+  --icon=./logo-launcher.ico `
+  --app-version=$env:npm_package_version-$($hash.Substring(0,8))
+cp ./logo-launcher.ico ./pack/icon.ico
